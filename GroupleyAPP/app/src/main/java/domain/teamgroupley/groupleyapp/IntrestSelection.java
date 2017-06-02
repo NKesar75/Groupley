75f378a2075f943497b4993bc1ef82436b4f9712
Raj Chandan <RPChandan@fullsail.edu>
2017-06-01 23:02:21 -0400

    Menu button in insert works

--- GroupleyAPP/app/src/main/java/domain/teamgroupley/groupleyapp/IntrestSelection.java
+++ GroupleyAPP/app/src/main/java/domain/teamgroupley/groupleyapp/IntrestSelection.java
@@ -1,17 +1,29 @@
 package domain.teamgroupley.groupleyapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class IntrestSelection extends AppCompatActivity {
-    
+
+    public Button btn1;
+    public void newpage()
+    {
+        btn1 = (Button) findViewById(R.id.menu_btn);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent changepage = new Intent(IntrestSelection.this,Createlogin.class);
+                startActivity(changepage);
+            }
+        });
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intrest_selection);
-
-
+        newpage();
     }
 }

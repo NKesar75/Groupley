06a135e88906ad5cd11e977e39d16e6aab3568e6
Nilesh Kesar <NKesar@fullsail.edu>
2017-06-02 00:22:54 -0400

    Finished the intrests list for now

--- GroupleyAPP/app/src/main/java/domain/teamgroupley/groupleyapp/MenuPage.java
+++ GroupleyAPP/app/src/main/java/domain/teamgroupley/groupleyapp/MenuPage.java
@@ -1,13 +1,30 @@
 package domain.teamgroupley.groupleyapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuPage extends AppCompatActivity {
+
+    public Button btn1;
+    public void newpage()
+    {
+        btn1 = (Button) findViewById(R.id.INTREST_BTN);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent changepage = new Intent(MenuPage.this,IntrestSelection.class);
+                startActivity(changepage);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_page);
+            newpage();
     }
 }

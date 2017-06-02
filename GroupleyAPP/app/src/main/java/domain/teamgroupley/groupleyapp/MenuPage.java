053922629f4905b84a756417ae4d5b28b847d27a
Nilesh Kesar <NKesar@fullsail.edu>
2017-06-01 22:48:14 -0400

    the new files added

--- GroupleyAPP/app/src/main/java/domain/teamgroupley/groupleyapp/MenuPage.java
+++ GroupleyAPP/app/src/main/java/domain/teamgroupley/groupleyapp/MenuPage.java
@@ -2,16 +2,12 @@ package domain.teamgroupley.groupleyapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class IntrestSelection extends AppCompatActivity {
-    
+public class MenuPage extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intrest_selection);
-
-
+        setContentView(R.layout.activity_menu_page);
     }
 }

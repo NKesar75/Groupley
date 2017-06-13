186bb1c1675659ba9923072f19a2e9f0ab5c4387
Raj Chandan <RPChandan@fullsail.edu>
2017-06-13 19:26:44 -0400

    Create Event done Create interest done old menu added navigation removed

--- GroupleyAPP/app/src/main/java/domain/teamgroupley/groupleyapp/CreateEventFragment.java
+++ GroupleyAPP/app/src/main/java/domain/teamgroupley/groupleyapp/CreateEventFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 /**
@@ -23,7 +25,14 @@ public class CreateEventFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_event, container, false);
+        View view = inflater.inflate(R.layout.fragment_create_event, container, false);
+        Spinner mySpinner = (Spinner) view.findViewById(R.id.Category_SPINNER);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.Category));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
+
+        return view;
     }
 
 }
--- GroupleyAPP/app/src/main/java/domain/teamgroupley/groupleyapp/ProfileFragment.java
+++ GroupleyAPP/app/src/main/java/domain/teamgroupley/groupleyapp/ProfileFragment.java
@@ -1,11 +1,19 @@
 package domain.teamgroupley.groupleyapp;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 /**
@@ -13,7 +21,10 @@ import android.view.ViewGroup;
  */
 public class ProfileFragment extends Fragment {
 
-
+    private Button Logout;
+    private static final String TAG = "ProfileFragment";
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
     public ProfileFragment() {
         // Required empty public constructor
     }
@@ -23,7 +34,45 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+        Logout = (Button)view.findViewById(R.id.logout_btn);
+        Logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getActivity(), LoginUser.class);
+                in.putExtra("some","some data");
+                startActivity(in);
+            }
+        });
+        return view;
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
 }

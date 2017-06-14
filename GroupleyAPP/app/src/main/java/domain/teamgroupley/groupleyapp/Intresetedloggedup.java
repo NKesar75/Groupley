7c8bcc8345bd103389629de993388def1b11533a
Nilesh Kesar <NKesar@fullsail.edu>
2017-06-14 16:31:20 -0400

    Finshed Create Event

--- GroupleyAPP/app/src/main/java/domain/teamgroupley/groupleyapp/Intresetedloggedup.java
+++ GroupleyAPP/app/src/main/java/domain/teamgroupley/groupleyapp/Intresetedloggedup.java
@@ -9,11 +9,163 @@ import android.widget.CheckBox;
 public class Intresetedloggedup {
 
 
+    private String SmArchery;
+    private String SmBaseball;
+    private String SBasketball;
+    private String SCycling;
+    private String SFootball;
+    private String SFrisbe;
+    private String SGolf;
+    private String SHoccey;
+    private String SHunting;
+    private String SSkateboarding;
+    private String SSnowBoarding;
+    private String SWaterSports;
+    private String SWrestling;
+    private String PFestivles;
+    private String PHouseParites;
+    private String PNightClubs;
+    private String GAction;
+    private String GAdventure;
+    private String GFPS;
+    private String GIndies;
+    private String GMMO;
+    private String GPartfam;
+    private String GRPG;
+    private String GSim;
+    private String GSports;
+    private String GStragy;
+    private String MCountry;
+    private String MDrillrap;
+    private String MEDM;
+    private String MJAZZ;
+    private String MRap;
+    private String MRock;
+    private String MRNB;
+    private String MScremo;
+    private String MoAction;
+    private String MoAnimation;
+    private String MoComdey;
+    private String MoDoc;
+    private String MoFam;
+    private String MoHorror;
+    private String MoMusical;
+    private String MoSifi;
+    private String MoSports;
+    private String MoThriller;
+    private String MoWar;
+    private String TAction;
+    private String TAdventure;
+    private String TAnimation;
+    private String TBiography;
+    private String TComedy;
+    private String TCrime;
+    private String TDoc;
+    private String TDrama;
+    private String TFam;
+    private String TGameShows;
+    private String THistory;
+    private String THorror;
+    private String TMystery;
+    private String TReal;
+    private String TSit;
+    private String TSports;
+    private String TTalkShows;
+    private String TWar;
+    private String DActing;
+    private String DCosplay;
+    private String DLarp;
+    private String CActionfigs;
+    private String CCars;
+    private String CCoins;
+    private String CComics;
+    private String CGuns;
+    private String CTrucks;
 
 
 
 
-    Intresetedloggedup(){
+    Intresetedloggedup(String SArc, String SBas, String SbK, String Cyc, String Foot, String Fris, String SGof, String Shockey,
+                       String SHunt, String SSKate, String SSnow, String Sws, String Wre, String Fes, String Hous, String Night,
+                       String Gact, String Gadv, String GFp, String Gind, String GMM, String GpaF, String GRP, String Gsi,
+                       String Gsp, String GStr, String MC, String MDR, String MEd, String MJz, String MRp, String Mro, String MRN,
+                       String MScr, String MoAct, String MOAni, String MOCom, String MODo, String MOF, String MOHOr, String MoMus,
+                       String MOSi, String MOSpo, String MOTHril, String MoWa, String TAct, String TADv, String TAni, String TBio,
+                       String TCom, String TCri, String TDo, String TDra, String Tfa, String TGame, String THis, String Thor, String TMys,
+                       String Tre, String Tsi, String TSpo, String TTalk, String Twa, String Dact, String Dcos, String Dla, String CAct,
+                       String CCr, String Ccins, String Ccom, String CGun, String Ctrc){
 
-}
+        SmArchery = SArc;
+        SmBaseball = SBas;
+        SBasketball = SbK;
+        SCycling = Cyc;
+        SFootball = Foot;
+        SFrisbe = Fris;
+        SGolf = SGof;
+        SHoccey = Shockey;
+        SHunting = SHunt;
+        SSkateboarding = SSKate;
+        SSnowBoarding = SSnow;
+        SWaterSports = Sws;
+        SWrestling = Wre;
+        PFestivles = Fes;
+        PHouseParites = Hous;
+        PNightClubs = Night;
+        GAction = Gact;
+        GAdventure = Gadv;
+        GFPS = GFp;
+        GIndies = Gind;
+        GMMO = GMM;
+        GPartfam = GpaF;
+        GRPG = GRP;
+        GSim = Gsi;
+        GSports = Gsp;
+        GStragy = GStr;
+        MCountry = MC;
+        MDrillrap = MDR;
+        MEDM = MEd;
+        MJAZZ = MJz;
+        MRap = MRp;
+        MRock = Mro;
+        MRNB = MRN;
+        MScremo = MScr;
+        MoAction = MoAct;
+        MoAnimation = MOAni;
+        MoComdey = MOCom;
+        MoDoc = MODo;
+        MoFam = MOF;
+        MoHorror = MOHOr;
+        MoMusical = MoMus;
+        MoSifi = MOSi;
+        MoSports = MOSpo;
+        MoThriller = MOTHril;
+        MoWar = MoWa;
+        TAction = TAct;
+        TAdventure = TADv;
+        TAnimation = TAni;
+        TBiography = TBio;
+        TComedy = TCom;
+        TCrime = TCri;
+        TDoc = TDo;
+        TDrama = TDra;
+        TFam = Tfa;
+        TGameShows = TGame;
+        THistory = THis;
+        THorror = Thor;
+        TMystery = TMys;
+        TReal = Tre;
+        TSit = Tsi;
+        TSports = TSpo;
+        TTalkShows = TTalk;
+        TWar = Twa;
+        DActing = Dact;
+        DCosplay = Dcos;
+        DLarp = Dla;
+        CActionfigs = CAct;
+        CCars = CCr;
+        CCoins = Ccins;
+        CComics = Ccom;
+        CGuns = CGun;
+        CTrucks = Ctrc;
+    }
 }
